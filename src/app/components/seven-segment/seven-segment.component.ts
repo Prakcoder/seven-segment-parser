e168de3e5e141e755bade06c6beecbbeec568e43
Prakcoder <Prakashverma042@gmail.com>
2019-04-16 10:12:01 +0530

    Implemented code to handle input and parsing the numbers

--- src/app/components/seven-segment/seven-segment.component.ts
+++ src/app/components/seven-segment/seven-segment.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 
+import { parseInputIntoNumbers } from '../../functions/seven-segment.utils';
+import { DataService } from '../../services/data.service';
+
 @Component({
-  selector: 'app-seven-segment',
-  templateUrl: './seven-segment.component.html',
-  styleUrls: ['./seven-segment.component.scss']
+    selector: 'app-seven-segment',
+    templateUrl: './seven-segment.component.html',
+    styleUrls: ['./seven-segment.component.scss']
 })
 export class SevenSegmentComponent implements OnInit {
 
-  constructor() { }
+    public fileData: string;
+    public output: string;
+    public file: File;
+    public outputFile: File;
+    private correctOutput: string;
+    public testResult: string;
 
-  ngOnInit() {
-  }
+    constructor(private dataService: DataService) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    private reset() {
+        this.output = undefined;
+        this.outputFile = undefined;
+        this.testResult = undefined;
+    }
+
+    readInputFile(event) {
+        this.reset();
+        this.file = event.target.files[0];
+        const reader = this.dataService.readFileInput(this.file);
+        reader.onload = (e) => {
+            this.fileData = reader.result as string;
+        };
+    }
+
+    parseInput() {
+        this.output = parseInputIntoNumbers(this.fileData);
+    }
+
+    readTestFile(event) {
+        this.outputFile = event.target.files[0];
+        const reader = this.dataService.readFileInput(this.outputFile);
+        reader.onload = (e) => {
+            this.correctOutput = reader.result as string;
+        };
+    }
+
+    testOutput() {
+        this.testResult = this.output === this.correctOutput ? 'PASS' : 'Fail';
+    }
 
 }
